214effdd448a8f1ac3ffe796321d1949c3354266
hgirish <girish@post.com>
2020-11-08 20:04:19 -0800

    Added AutoMapper mapping

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -1,4 +1,6 @@
+using AutoMapper;
 using CommandAPI.Data;
+using CommandAPI.Dtos;
 using CommandAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
@@ -10,26 +12,29 @@ namespace CommandAPI.Controllers
   public class CommandsController : ControllerBase
   {
     private readonly ICommandAPIRepo _repository;
-    public CommandsController(ICommandAPIRepo repository)
+    private readonly IMapper _mapper;
+
+    public CommandsController(ICommandAPIRepo repository, IMapper mapper)
     {
       this._repository = repository;
-
+      this._mapper = mapper;
     }
     [HttpGet]
-    public ActionResult<IEnumerable<string>> Get()
+    public ActionResult<IEnumerable<CommandReadDto>> GetAllCommands()
     {
       var commandItems = _repository.GetAllCommands();
-      return Ok(commandItems);
+      var dtos = _mapper.Map<IEnumerable<CommandReadDto>>(commandItems);
+      return Ok(dtos);
     }
     [HttpGet("{id}")]
-    public ActionResult<Command> GetCommandById(int id)
+    public ActionResult<CommandReadDto> GetCommandById(int id)
     {
       var commandItem = _repository.GetCommandById(id);
       if (commandItem == null)
       {
         return NotFound();
       }
-      return Ok(commandItem);
+      return Ok(_mapper.Map<CommandReadDto>(commandItem));
     }
   }
 }
